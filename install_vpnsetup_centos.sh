fc411442e4073e05cf68a77457dc08b074ffec83
chuckisbusy <chuckisbusy@icloud.com>
2022-04-21 14:08:25 -0400

    Delete install_vpnsetup_centos.sh

--- install_vpnsetup_centos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# All values MUST be placed inside 'single quotes'
-# DO NOT use these special characters within values: \ " '
-wget https://git.io/vpnsetup-centos -O vpnsetup.sh && sudo \
-VPN_IPSEC_PSK='LjiYwn4PnqiAHdjm' \
-VPN_USER='chuck' \
-VPN_PASSWORD='steve222' \
-sh vpnsetup.sh
